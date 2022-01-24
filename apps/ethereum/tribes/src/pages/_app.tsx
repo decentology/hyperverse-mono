6e73e0fc21c835ed886ef796609a2cc9ea558649
Angelica Turla <turla_angelica@yahoo.com>
2022-01-23 20:15:20 -0500

    Contract address comment added in _app

--- apps/ethereum/tribes/src/pages/_app.tsx
+++ apps/ethereum/tribes/src/pages/_app.tsx
@@ -1,3 +1,5 @@
+//Contract Address : 0x410E22b393B3A90953c0677F2282E331580ed45b
+
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import * as Hyperverse from "@decentology/hyperverse";
