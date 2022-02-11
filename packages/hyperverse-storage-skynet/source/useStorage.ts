90c4583645f6fbd0b7830f8a4edafaec8726ef1a
Jonathan Sheely <jsheely@thirtytech.net>
2022-02-10 22:34:11 -0500

    Update storage to provide link capabilities

--- packages/hyperverse-storage-skynet/source/useStorage.ts
+++ packages/hyperverse-storage-skynet/source/useStorage.ts
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useMemo, useState } from "react";
 import { SkynetClient } from "skynet-js";
 import { createContainer } from "unstated-next";
 
@@ -11,12 +11,16 @@ function StorageState(
 ) {
   const [client] = useState<SkynetClient>(new SkynetClient(clientUrl));
   const { uploadFile, uploadDirectory, downloadFile, openFile } = client;
+  const getLink = (siaLink: string) =>
+    `${clientUrl}/${siaLink.replace("sia:", "")}`;
   return {
     uploadFile: uploadFile.bind(client),
     uploadDirectory: uploadDirectory.bind(client),
     downloadFile: downloadFile.bind(client),
     openFile: openFile.bind(client),
+    getLink: getLink,
     client,
+    clientUrl
   };
 }
 
