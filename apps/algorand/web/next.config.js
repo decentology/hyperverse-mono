25c867c571da5f47dbe495320f134c732f698a82
Jonathan Sheely <jsheely@thirtytech.net>
2022-02-26 12:29:12 -0500

    Updated next.config to auto compile

--- apps/algorand/web/next.config.js
+++ apps/algorand/web/next.config.js
@@ -1,16 +1,18 @@
-const withTM = require("next-transpile-modules")([
-  "@decentology/hyperverse",
-  "@decentology/hyperverse-algorand",
-  "@decentology/hyperverse-algorand-counter",
+const withTM = require('next-transpile-modules')([
+	'@decentology/web3modal',
+	'@decentology/unstated-next',
+	'@decentology/hyperverse',
+	'@decentology/hyperverse-algorand',
+	'@decentology/hyperverse-algorand-counter',
 ]);
 
 module.exports = withTM({
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module = {
-      ...config.module,
-      noParse: /(gun|sea)\.js$/,
-    };
-    return config;
-  },
+	reactStrictMode: true,
+	webpack: (config) => {
+		config.module = {
+			...config.module,
+			noParse: /(gun|sea)\.js$/,
+		};
+		return config;
+	},
 });
--- apps/flow/tribes/next.config.js
+++ apps/flow/tribes/next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const withTM = require('next-transpile-modules')([
-  '@decentology/hyperverse',
-  '@decentology/hyperverse-flow',
-  '@decentology/hyperverse-flow-tribes',
-])
+	'@decentology/web3modal',
+	'@decentology/unstated-next',
+	'@decentology/hyperverse',
+	'@decentology/hyperverse-flow',
+	'@decentology/hyperverse-flow-tribes',
+]);
 
 module.exports = withTM({
-  reactStrictMode: true,
-  images: {
-    domains: ['ipfs.infura.io']
-  }
-})
+	reactStrictMode: true,
+	images: {
+		domains: ['ipfs.infura.io'],
+	},
+});
