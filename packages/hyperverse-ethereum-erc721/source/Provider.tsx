c13596a550d4225933004dbc2887a4c13726ded6
jacob-tucker <jacobtucker818@gmail.com>
2022-02-25 21:05:26 -0600

    remove error

--- packages/hyperverse-ethereum-erc721/source/Provider.tsx
+++ packages/hyperverse-ethereum-erc721/source/Provider.tsx
@@ -1,12 +1,9 @@
 import { FC } from 'react';
 import { QueryClientProvider, QueryClient } from 'react-query';
-import ABI from '../artifacts/contracts/ERC721.sol/ERC721.json';
 import { HyperverseModuleInstance, networks, useHyperverse } from '@decentology/hyperverse';
 import { ERC721 } from './useERC721';
 const client = new QueryClient();
 
-export const ContractABI = ABI.abi;
-export const CONTRACT_TESTNET_ADDRESS = '0xf8CEc073d08e42cdDC1bF1fd8d44ce3252ab7352';
 export const TENANT_TESTNET_ADDRESS = '0xD847C7408c48b6b6720CCa75eB30a93acbF5163D';
 
 const Provider: FC<HyperverseModuleInstance> = ({ children, tenantId }) => {
