599160b78df4ec72501b97d76a2cf507dd1e372d
Jonathan Sheely <jsheely@thirtytech.net>
2022-02-28 16:36:50 -0500

    Converting Tribes to EVM

---
+++ packages/hyperverse-evm-tribes/hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`,
+      accounts: [process.env.NEXT_PRIVATE_KEY],
+    },
+    metis: {
+      url: "https://stardust.metis.io/?owner=588",
+      accounts:
+        process.env.NEXT_PRIVATE_KEY !== undefined ? [process.env.NEXT_PRIVATE_KEY] : [],
+    },
+    avalanche: {
+      url: "",
+      accounts:
+        process.env.NEXT_PRIVATE_KEY !== undefined ? [process.env.NEXT_PRIVATE_KEY] : [],
+    },
+  },
+};
---
+++ packages/hyperverse-evm-tribes/source/environment.ts
@@ -0,0 +1,41 @@
+import { networks, useHyperverse, blockchains, } from '@decentology/hyperverse';
+
+const environment = {
+	[blockchains.Ethereum]: {
+		[networks.Mainnet]: {
+			contractAddress: null,
+		},
+		[networks.Testnet]: {
+			contractAddress: '0xf8CEc073d08e42cdDC1bF1fd8d44ce3252ab7352',
+			tenantAddress: '0xD847C7408c48b6b6720CCa75eB30a93acbF5163D'
+		},
+	},
+	[blockchains.Metis]: {
+		[networks.Mainnet]: {
+			contractAddress: null,
+		},
+		[networks.Testnet]: {
+			contractAddress: 'x07F3062D51C9A6CA568C8135656054FA88D5b646',
+			tenantAdress: '0x8f8B8BE836fbe857c65E892dBb261F249f9b0adb'
+		},
+	},
+	[blockchains.Avalanche]: {
+		[networks.Mainnet]: {
+			contractAddress: null,
+		},
+		[networks.Testnet]: {
+			contractAddress: ''
+		}
+	}
+}
+
+function useEnvironment() {
+	const { blockchain, network } = useHyperverse();
+	if (blockchain == null) {
+		throw new Error('Blockchain is not set');
+	}
+	// @ts-ignore -- TODO: Properly type this index lookup
+	return environment[blockchain.name][network];
+}
+
+export { environment, useEnvironment };
