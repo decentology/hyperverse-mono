477d130f122fcaaafed0b5ff7246549d3728f757
Angelica Turla <turla_angelica@yahoo.com>
2022-02-28 02:29:15 -0500

    updates to module

--- packages/hyperverse-ethereum-stake-rewards/source/constants.ts
+++ packages/hyperverse-ethereum-stake-rewards/source/constants.ts
@@ -1,7 +1,7 @@
-import StakeFactory from '../artifacts/contracts/StakeRewardsFactory.sol/StakeRewardsFactory.json';
 import Stake from '../artifacts/contracts/StakeRewardsToken.sol/StakeRewardsToken.json';
+import StakeFactory from '../artifacts/contracts/StakeRewardsFactory.sol/StakeRewardsFactory.json';
 export const STAKE_ABI = Stake.abi;
 export const FACTORY_ABI = StakeFactory.abi;
-export const STAKE_MAIN_ADDRESS = '0xAF1b5B3777509E2a016615bFc616c4C5875353A2';
-export const STAKE_FACTORY_ADDRESS = '0xf2C30C005d63572D357825624c5C78b004a0D079';
+export const STAKE_MAIN_ADDRESS = '0x0Dde9fD5868Ae7d229a98dF7d5DAd37FE0d00c54';
+export const STAKE_FACTORY_ADDRESS = '0x5541c1472FD5C037Cb733893E6D6aFFEDC844b25';
 export const TENANT_ADDRESS = '0x87DAD25225CaB96E1C9492CB43c41d70c85E6022';
