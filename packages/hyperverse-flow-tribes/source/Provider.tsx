bf18750ab3c96997a2530d098bf5e459caf41543
Jonathan Sheely <jsheely@thirtytech.net>
2022-02-28 08:02:47 -0500

    Null check

--- packages/hyperverse-flow-tribes/source/Provider.tsx
+++ packages/hyperverse-flow-tribes/source/Provider.tsx
@@ -3,5 +3,5 @@ import { FC } from 'react';
 import { Provider as TribesProvider } from './useTribes';
 
 export const Provider: FC<HyperverseModuleInstance> = ({ children, tenantId }) => {
-	return <TribesProvider initialState={{ tenantId }}>{children}</TribesProvider>;
+	return <TribesProvider initialState={{ tenantId: tenantId || '' }}>{children}</TribesProvider>;
 };
