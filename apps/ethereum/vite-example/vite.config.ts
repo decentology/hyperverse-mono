beaf7a72b6b68126e691a33cffda35c9a087bc3e
Jonathan Sheely <jsheely@thirtytech.net>
2022-03-07 15:05:00 -0500

    I don't know what I'm doing

--- apps/ethereum/vite-example/vite.config.ts
+++ apps/ethereum/vite-example/vite.config.ts
@@ -1,23 +1,21 @@
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-import refreshRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill';
 import polyfillNode from 'rollup-plugin-polyfill-node';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		polyfillNode(),
-		refreshRefresh(),
+		react(),
 
 	],
 	optimizeDeps: {
-		// include: [
-		// 	'@decentology/hyperverse',
-		// 	'@decentology/hyperverse-evm',
-		// 	'@decentology/web3modal',
-		// 	'@decentology/hyperverse-ethereum',
-		// 	'@decentology/hyperverse-ethereum-randompick'
-		// ],
+		include: [
+			// '@decentology/hyperverse',
+			// '@decentology/hyperverse-evm',
+			// '@decentology/web3modal',
+			// '@decentology/hyperverse-ethereum'
+		],
 		esbuildOptions: {
 			define: {
 				global: "globalThis"
@@ -30,25 +28,15 @@ export default defineConfig({
 		}
 		// exclude: ['@decentology/web3modal']
 	},
-	// build: {
-	// 	commonjsOptions: {
-	// 		include: [
-	// 			'@decentology/hyperverse',
-	// 			'@decentology/hyperverse-evm',
-	// 			'@decentology/web3modal',
-	// 			'@decentology/hyperverse-ethereum',
-	// 			'@decentology/hyperverse-ethereum-randompick'
-	// 		]
-	// 	}
-	// },
+	build: {
+		commonjsOptions: {
+			// include: ['@decentology/web3modal']
+		}
+	},
 	resolve: {
 		alias: {
 			'react/jsx-runtime': 'react/jsx-runtime.js',
-			'@decentology/hyperverse': 'hyperverse',
-			'@decentology/hyperverse-evm': 'hyperverse-evm',
-			'@decentology/web3modal': 'web3modal',
-			'@decentology/hyperverse-ethereum': 'hyperverse-ethereum',
-			'@decentology/hyperverse-ethereum-randompick': 'hyperverse-ethereum-randompick'	
+			// buffer: 'buffer'
 		}
 	},
 	define: {
