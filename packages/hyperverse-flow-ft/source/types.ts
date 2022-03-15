da1eee6956c009881731b295dc5df3bad02a0b2e
Jonathan Sheely <jsheely@thirtytech.net>
2022-03-15 09:45:17 -0400

    Missing type

--- packages/hyperverse-flow-ft/source/types.ts
+++ packages/hyperverse-flow-ft/source/types.ts
@@ -4,6 +4,12 @@ export type Bind1<Fn extends (...args: any) => any> = (
 	...args: Tail<Parameters<Fn>>
 ) => ReturnType<Fn>;
 
+export type TribesData = {
+	name: string;
+	ipfsHash: string;
+	description: string;
+};
+
 export type FlowTransaction = {
 	errorMessage: string;
 	events: FlowEvent[];
