85a546b12a7f19701139e5475dd2ec9e5aa8c51f
Jonathan Sheely <jsheely@thirtytech.net>
2022-03-16 14:16:35 -0400

    ERC 721 polygon supported

--- packages/hyperverse-evm-erc721/hardhat.config.js
+++ packages/hyperverse-evm-erc721/hardhat.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 require('@nomiclabs/hardhat-waffle');
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
@@ -16,6 +17,9 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const accounts = process.env.NEXT_PRIVATE_KEY !== undefined ? [process.env.NEXT_PRIVATE_KEY] : [];
+
 module.exports = {
 	solidity: '0.8.4',
 	defaultNetwork: 'hardhat',
@@ -23,18 +27,19 @@ module.exports = {
 		hardhat: {},
 		ethereum: {
 			url: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`,
-			accounts:
-				process.env.NEXT_PRIVATE_KEY !== undefined ? [process.env.NEXT_PRIVATE_KEY] : [],
+			accounts,
 		},
 		metis: {
 			url: 'https://stardust.metis.io/?owner=588',
-			accounts:
-				process.env.NEXT_PRIVATE_KEY !== undefined ? [process.env.NEXT_PRIVATE_KEY] : [],
+			accounts,
 		},
 		avalanche: {
 			url: 'https://api.avax-test.network/ext/bc/C/rpc',
-			accounts:
-				process.env.NEXT_PRIVATE_KEY !== undefined ? [process.env.NEXT_PRIVATE_KEY] : [],
+			accounts,
+		},
+		polygon: {
+			url: 'https://rpc-mumbai.maticvigil.com',
+			accounts,
 		},
 	},
 };
