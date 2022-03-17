8d15594e71027bea3bcecd33630cebb05beb71f6
Angelica Turla <turla_angelica@yahoo.com>
2022-03-16 22:52:16 -0400

    started from new erc777

---
+++ packages/hyperverse-evm-erc777/test/token-test.js
@@ -0,0 +1,75 @@
+const { expect } = require('chai');
+const { BigNumber } = require('ethers');
+const { ethers } = require('hardhat');
+
+let unitMultiple = new BigNumber.from(10).pow(new BigNumber.from(18));
+let initialSupply = new BigNumber.from(1000000).mul(unitMultiple);
+
+describe('Token', function () {
+	let ERC777;
+	let erc777ctr;
+	let TokenFactory;
+	let tokenFactoryCtr;
+	let alice;
+	let bob;
+	let cara;
+	let aliceProxyContract;
+
+	const aliceInstance = {
+		name: 'ALICE',
+		symbol: 'ALC',
+		initialSupply: initialSupply,
+	};
+
+	beforeEach(async () => {
+		ERC777 = await ethers.getContractFactory('ERC777');
+		[owner, alice, bob, cara] = await ethers.getSigners();
+
+		erc777ctr = await ERC777.deploy(owner.address);
+		await erc777ctr.deployed();
+
+		TokenFactory = await ethers.getContractFactory('ERC777Factory');
+		tokenFactoryCtr = await TokenFactory.deploy(erc777ctr.address, owner.address);
+		await tokenFactoryCtr.deployed();
+	
+	
+		await tokenFactoryCtr
+		.connect(alice)
+		.createInstance(
+			aliceInstance.name,
+			aliceInstance.symbol,
+			[],
+			aliceInstance.initialSupply,
+			alice.address
+		);
+
+
+		const main = await ethers.getContractFactory('ERC777');
+		aliceProxyContract = await main.attach(await tokenFactoryCtr.getProxy(alice.address));
+	});
+
+	describe('Initial State Variables', function () {
+		it('Master Contract should match erc777ctr', async function () {
+			expect(await tokenFactoryCtr.masterContract()).to.equal(erc777ctr.address);
+		});
+
+		it("Should match alice's initial token data", async function () {
+			expect(await aliceProxyContract.name()).to.equal(aliceInstance.name);
+			expect(await aliceProxyContract.symbol()).to.equal(aliceInstance.symbol);
+			expect(await aliceProxyContract.decimals()).to.equal(18);
+			expect(await aliceProxyContract.granularity()).to.equal(1);
+		});
+
+		it("Should assign all initial token to Alice", async function () {
+			const aliceTotal = await aliceProxyContract.balanceOf(alice.address);
+			expect(await aliceProxyContract.totalSupply()).to.equal(aliceTotal);
+		})
+
+		it("Should have Alice's proxy contract as a default operator", async function () {
+			const defualtOps = await aliceProxyContract.defaultOperators();
+			expect(defualtOps[0]).to.equal(aliceProxyContract.address);
+		})
+	});
+
+
+});
