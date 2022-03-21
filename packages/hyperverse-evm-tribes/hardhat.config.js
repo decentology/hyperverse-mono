38b03401c50b6da4e3ef99bf056b9f1b60113157
Jonathan Sheely <jsheely@thirtytech.net>
2022-03-21 09:44:34 -0400

    Making stories great again

--- packages/hyperverse-evm-tribes/hardhat.config.js
+++ packages/hyperverse-evm-tribes/hardhat.config.js
@@ -24,22 +24,24 @@ module.exports = {
 	solidity: '0.8.4',
 	defaultNetwork: 'hardhat',
 	networks: {
-		hardhat: {},
+		hardhat: {
+			chainId: 4
+		},
 		ethereum: {
 			url: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`,
-			accounts,
+			accounts
 		},
 		metis: {
 			url: 'https://stardust.metis.io/?owner=588',
-			accounts,
+			accounts
 		},
 		avalanche: {
 			url: 'https://api.avax-test.network/ext/bc/C/rpc',
-			accounts,
+			accounts
 		},
 		polygon: {
 			url: 'https://rpc-mumbai.maticvigil.com',
-			accounts,
-		},
-	},
+			accounts
+		}
+	}
 };
