840079ce1f50fb3718bf0d318328729fa010fe02
Jonathan Sheely <jsheely@thirtytech.net>
2022-03-25 09:53:45 -0400

    Clean up

--- packages/hyperverse-evm-tribes/stories/utils/Provider.tsx
+++ packages/hyperverse-evm-tribes/stories/utils/Provider.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import { initialize, Network, Provider } from '@decentology/hyperverse';
-import { Localhost, Evm } from '@decentology/hyperverse-evm';
+import { Localhost } from '@decentology/hyperverse-evm';
 import * as Tribes from '../../source';
-import { hexlify } from 'ethers/lib/utils';
 
 export const HyperverseProvider = ({ children }) => {
 	const hyperverse = initialize({
@@ -11,9 +9,9 @@ export const HyperverseProvider = ({ children }) => {
 			type: Network.Testnet,
 			chainId: 1337,
 			name: 'localhost',
-			networkUrl: 'http://localhost:6006/hyperchain',
+			networkUrl: 'http://localhost:6006/hyperchain'
 		},
-		modules: [{ bundle: Tribes, tenantId: '0x62a7aa79a52591Ccc62B71729329A80a666fA50f' }],
+		modules: [{ bundle: Tribes, tenantId: '0x62a7aa79a52591Ccc62B71729329A80a666fA50f' }]
 	});
 	return <Provider initialState={hyperverse}>{children}</Provider>;
 };
