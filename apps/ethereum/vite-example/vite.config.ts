74fb3fb42ff3171bf51c4f3426a859fe17d00956
Jonathan Sheely <jsheely@thirtytech.net>
2022-03-26 21:28:19 -0400

    I got it to work. Saving before it fails

--- apps/ethereum/vite-example/vite.config.ts
+++ apps/ethereum/vite-example/vite.config.ts
@@ -2,47 +2,48 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill';
 import polyfillNode from 'rollup-plugin-polyfill-node';
-
+import * as React from 'react';
 export default defineConfig({
 	plugins: [
 		polyfillNode(),
 		react(),
-
 	],
+	build: {
+		commonjsOptions: {
+			include: [/@decentology/,],
+		},
+	},
 	optimizeDeps: {
+		// entries: ['../../../packages/**/*'],
 		include: [
 			// '@decentology/hyperverse',
+			// '@decentology/hyperverse-ethereum',
+			// '@decentology/hyperverse-ethereum-randompick',
+			// '@decentology/unstated-next',
 			// '@decentology/hyperverse-evm',
-			// '@decentology/web3modal',
-			// '@decentology/hyperverse-ethereum'
 		],
+		// exclude: ['react', 'react-dom'],
 		esbuildOptions: {
 			define: {
-				global: "globalThis"
+				global: 'globalThis',
 			},
 			plugins: [
 				NodeGlobalsPolyfillPlugin({
-					buffer: true
-				})
-			]
-		}
-		// exclude: ['@decentology/web3modal']
-	},
-	build: {
-		commonjsOptions: {
-			// include: ['@decentology/web3modal']
-		}
+					buffer: true,
+				}),
+			],
+		},
 	},
 	resolve: {
 		alias: {
 			'react/jsx-runtime': 'react/jsx-runtime.js',
-			// buffer: 'buffer'
-		}
+			React: 'react',
+		},
 	},
 	define: {
-		global: {},
-		process: {
+		process : {
 			env: {}
-		}
+		},
+		React: React
 	}
 });
