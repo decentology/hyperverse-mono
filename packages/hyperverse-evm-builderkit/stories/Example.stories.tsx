930c0ba95d84974ae67f3dcb950a79858abaafef
Jonathan Sheely <jsheely@thirtytech.net>
2022-03-31 09:44:00 -0400

    Clean up

--- packages/hyperverse-evm-builderkit/stories/Example.stories.tsx
+++ packages/hyperverse-evm-builderkit/stories/Example.stories.tsx
@@ -1,13 +1,16 @@
 import { HyperverseProvider } from './utils/Provider';
 import { useModule } from '../source';
+import { useHyperverse } from '@decentology/hyperverse/source';
+import { useEvm } from '@decentology/hyperverse-evm/source';
 const Button = () => {
-	const {factoryContract } = useModule();
-	return <button>{ factoryContract.address}</button>;
-}
+	const { connect } = useEvm();
+	const { factoryContract } = useModule();
+	return <button onClick={() => connect()}>{factoryContract.address}</button>;
+};
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-	title: 'Example/Button',
+	title: 'Example/Test1',
 	component: Button,
 	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 };
@@ -19,6 +22,6 @@ const Template = (args) => (
 	</HyperverseProvider>
 );
 
-export const Account = Template.bind({});
+export const Example = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Account.args = {};
+Example.args = {};
