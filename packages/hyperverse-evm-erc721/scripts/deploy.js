368a7b211e2ad41a51ecdace40cc99a84e2f08db
Angelica Turla <turla_angelica@yahoo.com>
2022-04-12 21:20:59 -0400

    erc721 deploy testing

--- packages/hyperverse-evm-erc721/scripts/deploy.js
+++ packages/hyperverse-evm-erc721/scripts/deploy.js
@@ -37,8 +37,10 @@ async function main() {
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-	.then(() => process.exit(0))
+	// .then(() => process.exit(0))
 	.catch((error) => {
 		console.error(error);
 		process.exit(1);
 	});
+
+module.exports = { main }
