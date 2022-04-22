c870875ec2047fc6f4a21025348de6c523293bc8
Jonathan Sheely <jsheely@thirtytech.net>
2022-04-22 12:53:08 -0400

    Sample of changes made to storybook

--- packages/hyperverse-evm-tribes/stories/getAllTribes.tsx
+++ packages/hyperverse-evm-tribes/stories/getAllTribes.tsx
@@ -1,15 +1,18 @@
 import * as PropTypes from 'prop-types';
 import './button.css';
-import { useTribes } from '../source';
+import { Tribes, useTribes } from '../source';
+import { useEffect, useState } from 'react';
 
 export const GetAllTribes = ({ ...props }) => {
-	const { Tribes } = useTribes();
-	const { data: tribes } = Tribes();
-	console.log(tribes);
+	const tribes = useTribes();
+	const [data, setData] = useState(null);
+	useEffect(() => {
+		tribes.getAllTribes().then(setData);
+	}, []);
 
 	return (
 		<div className="tribes">
-			All Tribes: <b>{tribes}</b>
+			All Tribes: <b>{data}</b>
 		</div>
 	);
 };
