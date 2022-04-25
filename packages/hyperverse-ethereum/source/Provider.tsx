b5e51bb640db866046e4fd2c286038f46e747210
Jonathan Sheely <jsheely@thirtytech.net>
2022-04-25 11:04:43 -0400

    Formatting

--- packages/hyperverse-ethereum/source/Provider.tsx
+++ packages/hyperverse-ethereum/source/Provider.tsx
@@ -5,23 +5,23 @@ import { Network } from '@decentology/hyperverse';
 
 const INFURA_ID = process.env.INFURA_API_KEY! || 'fb9f66bab7574d70b281f62e19c27d49';
 export const Networks = {
-		[Network.Mainnet]: {
-			type: Network.Mainnet,
-			name: 'mainnet',
-			networkUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
-			chainId: 1
-		},
-		[Network.Testnet]: {
-			type: Network.Testnet,
-			name: 'rinkeby',
-			chainId: 4,
-			networkUrl: `https://rinkeby.infura.io/v3/${INFURA_ID}`,
-			explorerUrl: 'https://rinkeby.etherscan.io'
-		}
+	[Network.Mainnet]: {
+		type: Network.Mainnet,
+		name: 'mainnet',
+		networkUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+		chainId: 1,
+	},
+	[Network.Testnet]: {
+		type: Network.Testnet,
+		name: 'rinkeby',
+		chainId: 4,
+		networkUrl: `https://rinkeby.infura.io/v3/${INFURA_ID}`,
+		explorerUrl: 'https://rinkeby.etherscan.io',
+	},
 };
 const Provider: FC<any> = ({ children }) => {
 	return (
-		<Evm.Provider initialState={{networks: Networks}}>
+		<Evm.Provider initialState={{ networks: Networks }}>
 			<Ethereum.Provider>{children}</Ethereum.Provider>
 		</Evm.Provider>
 	);
