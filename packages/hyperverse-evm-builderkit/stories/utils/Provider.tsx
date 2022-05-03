4592328cbde1c89e6b4282bb506e68256fedd8a7
Jonathan Sheely <jsheely@thirtytech.net>
2022-05-03 15:00:06 -0400

    Not sure if this is final yet or not

--- packages/hyperverse-evm-builderkit/stories/utils/Provider.tsx
+++ packages/hyperverse-evm-builderkit/stories/utils/Provider.tsx
@@ -12,7 +12,7 @@ export const HyperverseProvider: FC<any> = ({ children }) => {
 			name: 'localhost',
 			networkUrl: 'http://localhost:6006/hyperchain'
 		},
-		modules: [{ bundle: SmartModule, tenantId: '0x62a7aa79a52591Ccc62B71729329A80a666fA50f' }]
+		modules: [{ bundle: SmartModule, tenantId: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266' }]
 	});
 	return <Provider initialState={hyperverse}>{children}</Provider>;
 };
