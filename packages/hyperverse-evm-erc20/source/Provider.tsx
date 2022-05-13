d1c873b43613710a4a2ddb86d12282be36242c39
Angelica Turla <turla_angelica@yahoo.com>
2022-05-13 15:58:33 -0400

    source files

--- packages/hyperverse-evm-erc20/source/Provider.tsx
+++ packages/hyperverse-evm-erc20/source/Provider.tsx
@@ -1,17 +1,13 @@
-import { HyperverseModuleInstance } from '@decentology/hyperverse';
 import { ERC20 } from './useERC20';
 import { FC } from 'react';
-import { QueryClientProvider, QueryClient } from 'react-query';
-const client = new QueryClient();
+import { HyperverseModuleInstance } from '@decentology/hyperverse';
 
 const Provider: FC<HyperverseModuleInstance> = ({ children, tenantId }) => {
 	if (!tenantId) {
 		throw new Error('Tenant ID is required');
 	}
 	return (
-		<QueryClientProvider client={client}>
 			<ERC20.Provider initialState={{ tenantId: tenantId }}>{children}</ERC20.Provider>
-		</QueryClientProvider>
 	);
 };
 
--- packages/hyperverse-evm-erc20/source/environment.ts
+++ packages/hyperverse-evm-erc20/source/environment.ts
@@ -1,33 +1,34 @@
 import {
-	Network,
-	Blockchain,
 	isEvm,
+	Blockchain,
 	BlockchainEvm,
 	EvmEnvironment,
-	useHyperverse,
+	NetworkConfig,
 } from '@decentology/hyperverse';
+import { ContractInterface } from 'ethers';
+import Contracts from '../contracts.json';
 import ERC20Factory from '../artifacts/contracts/ERC20Factory.sol/ERC20Factory.json';
 import ERC20 from '../artifacts/contracts/ERC20.sol/ERC20.json';
-export const ContractABI = ERC20.abi;
-export const FactoryABI = ERC20Factory.abi;
-import Contracts from '../contracts.json';
+
+export const ContractABI = ERC20.abi as ContractInterface;
+export const FactoryABI = ERC20Factory.abi as ContractInterface;
 
 const environment = Contracts as EvmEnvironment;
 
 
-function useEnvironment() {
-	const { blockchain, network } = useHyperverse();
-	if (blockchain == null) {
+function getEnvironment(blockchainName: Blockchain, network: NetworkConfig) {
+	if (blockchainName == null) {
 		throw new Error('Blockchain is not set');
 	}
-	if (!isEvm(blockchain?.name)) {
+	if (!isEvm(blockchainName)) {
 		throw new Error('Blockchain is not EVM compatible');
 	}
 
-	const chain = environment[blockchain.name as BlockchainEvm];
+	const chain = environment[blockchainName as BlockchainEvm];
 	if (!chain) {
 		throw new Error('Blockchain is not supported');
 	}
+
 	const env = chain[network.type];
 	return {
 		...env,
@@ -36,4 +37,4 @@ function useEnvironment() {
 	};
 }
 
-export { environment, useEnvironment };
+export { environment, getEnvironment };
