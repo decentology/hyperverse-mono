683f541397361b53c092d9c9f8eb08c0f534ee43
Angelica Turla <turla_angelica@yahoo.com>
2022-05-17 14:31:27 -0400

    erc777 update

--- packages/hyperverse-evm-erc777/source/index.ts
+++ packages/hyperverse-evm-erc777/source/index.ts
@@ -1,2 +1,10 @@
 export { useERC777 } from './useERC777';
 export { Provider } from './Provider';
+
+export const ModuleName = 'ERC20';
+
+import { Provider } from './Provider';
+export const ERC20 = {
+	Provider,
+	ModuleName,
+};
