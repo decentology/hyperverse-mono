e12aa95ded40cb0af50570a6159a657ff4c1b5d1
Jonathan Sheely <jsheely@thirtytech.net>
2022-05-20 12:57:46 -0400

    Stabalized imports

--- packages/hyperverse-ethereum/source/Provider.tsx
+++ packages/hyperverse-ethereum/source/Provider.tsx
@@ -3,7 +3,7 @@ import { Provider as EvmProvider, ProviderProps} from '@decentology/hyperverse-e
 import { Ethereum } from './useEthereum';
 import { Network, NetworkConfig } from '@decentology/hyperverse';
 
-const INFURA_ID = process.env.INFURA_API_KEY! || 'fb9f66bab7574d70b281f62e19c27d49';
+const INFURA_ID = globalThis.process?.env?.INFURA_API_KEY! || 'fb9f66bab7574d70b281f62e19c27d49';
 export const Networks : {[key in Network] : NetworkConfig} = {
 	[Network.Mainnet]: {
 		type: Network.Mainnet,
