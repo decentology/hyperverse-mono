1066954277e8c099625642f61a7c616f808b549e
Angelica Turla <turla_angelica@yahoo.com>
2022-05-26 20:45:02 -0400

    updates

--- packages/hyperverse-evm-erc721/source/environment.ts
+++ packages/hyperverse-evm-erc721/source/environment.ts
@@ -6,15 +6,13 @@ import {
 	NetworkConfig,
 } from '@decentology/hyperverse';
 
-import Contracts from '../contracts.json';
-import { ContractInterface } from 'ethers';
 import ERC721FactoryABI from '../artifacts/contracts/ERC721Factory.sol/ERC721Factory.json';
 import ERC721ABI from '../artifacts/contracts/ERC721.sol/ERC721.json';
+import Contracts from '../contracts.json';
+import { ContractInterface } from 'ethers';
 
 export const FactoryABI = ERC721FactoryABI.abi as ContractInterface;
 export const ContractABI = ERC721ABI.abi as ContractInterface;
-
-
 
 const environment = Contracts as EvmEnvironment;
 
