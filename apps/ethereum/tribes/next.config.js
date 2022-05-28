eb626952158c1899b72cec9708228f18aa51cad5
Jonathan Sheely <jsheely@thirtytech.net>
2022-05-28 07:29:31 -0400

    I need two modules hoisted

--- apps/ethereum/tribes/next.config.js
+++ apps/ethereum/tribes/next.config.js
@@ -1,17 +1,9 @@
-const webpack = require('webpack');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
-const packageJSON = require('./package.json');
-const transpiledPackages = Object.keys(packageJSON.dependencies).filter((it) =>
-	it.includes('@decentology/')
-);
-const withTM = require('next-transpile-modules')(transpiledPackages);
-// const withTM = require('next-transpile-modules')([
-// 	'@decentology/unstated-next',
-// 	'@decentology/hyperverse',
-// 	'@decentology/hyperverse-ethereum',
-// 	'@decentology/hyperverse-evm-tribes',
-// ]);
-
+const withTM = require('next-transpile-modules')([
+	'@decentology/unstated-next',
+	'@decentology/hyperverse',
+	'@decentology/hyperverse-ethereum',
+	'@decentology/hyperverse-evm-tribes',
+]);
 
 module.exports = withTM({
 	reactStrictMode: true,
