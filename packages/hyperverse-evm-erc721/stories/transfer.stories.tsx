b2e879a6e53617acd60a47a07da3f29ce0b2635f
Kendyl Key <67116724+kendylkey@users.noreply.github.com>
2022-05-31 10:18:49 -0700

    more stuff

--- packages/hyperverse-evm-erc721/stories/transfer.stories.tsx
+++ packages/hyperverse-evm-erc721/stories/transfer.stories.tsx
@@ -22,7 +22,7 @@ const Template = (args) => (
 export const Demo = Template.bind({});
 
 Demo.args = {
-	from: '',
-	to: '',
+	from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+	to: '0x976EA74026E726554dB657fA54763abd0C3a0aa9',
 	tokenId: 1,
 };
