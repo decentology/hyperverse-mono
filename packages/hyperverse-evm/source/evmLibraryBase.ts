0ba6ccaec582f6c6cb2e8732ef3794660a0839e0
Jonathan Sheely <jsheely@thirtytech.net>
2022-05-31 09:39:18 -0400

    Fix error variable

--- packages/hyperverse-evm/source/evmLibraryBase.ts
+++ packages/hyperverse-evm/source/evmLibraryBase.ts
@@ -49,7 +49,7 @@ export async function EvmLibraryBase(
 
 	try {
 		proxyAddress = await factoryContract.getProxy(tenantId);
-	} catch (error) {
+	} catch (e) {
 		const err = new Error(`Failed to get proxy address for tenant ${tenantId}`);
 	 	error = err;
 	}
