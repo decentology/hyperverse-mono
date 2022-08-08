d7a5924a4efe35947a5acd138e20cd1f0292bf51
Jonathan Sheely <jsheely@thirtytech.net>
2022-08-08 14:51:43 -0400

    Updated gold list for react-query

--- apps/bsc/safuux/components/gold-list.tsx
+++ apps/bsc/safuux/components/gold-list.tsx
@@ -1,21 +1,25 @@
 import { useSafuu } from '@decentology/hyperverse-bsc-safuu/react';
 import { ChangeEventHandler, SyntheticEvent, useEffect, useState } from 'react';
-
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { TransactionReceipt } from '@ethersproject/abstract-provider';
 export const GoldListMint = () => {
 	const { isGoldListSaleActive, mintGoldList } = useSafuu();
 	const [isActive, setIsActive] = useState(false);
 	const [nodes, setNodes] = useState({ fullNode: 0, liteNode: 0 });
+	const {
+		mutate: mint,
+		isLoading,
+		isError,
+		error,
+		isSuccess
+	} = useMutation<TransactionReceipt, Error, typeof nodes>((nodes) =>
+		mintGoldList!(nodes.fullNode, nodes.liteNode)
+	);
 	useEffect(() => {
 		isGoldListSaleActive?.().then(setIsActive);
 	}, [isGoldListSaleActive]);
 	const handleMint = () => {
-		mintGoldList!(nodes.fullNode, nodes.liteNode)
-			.then(() => {
-				// Update UI
-			})
-			.catch((err) => {
-				// Update Error UI
-			});
+		mint(nodes);
 	};
 	const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
 		setNodes({ ...nodes, [e.target.name]: Number(e.target.value) });
@@ -40,6 +44,8 @@ export const GoldListMint = () => {
 				<button disabled={!isActive} onClick={handleMint}>
 					Mint
 				</button>
+				{isError && <div style={{ color: 'red' }}>{error?.message}</div>}
+				{isSuccess && <div style={{ color: 'green' }}>Your nodes have been allocated!</div>}
 			</div>
 		</div>
 	);
