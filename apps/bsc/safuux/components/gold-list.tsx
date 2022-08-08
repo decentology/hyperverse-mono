15613b126fcc1e4b8e2e8066098715d73be7dc3f
Jonathan Sheely <jsheely@thirtytech.net>
2022-08-08 15:27:16 -0400

    Added more react-query and loading states

--- apps/bsc/safuux/components/gold-list.tsx
+++ apps/bsc/safuux/components/gold-list.tsx
@@ -1,26 +1,23 @@
 import { useSafuu } from '@decentology/hyperverse-bsc-safuu/react';
-import { ChangeEventHandler, SyntheticEvent, useEffect, useState } from 'react';
+import { ChangeEventHandler, useState } from 'react';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import { TransactionReceipt } from '@ethersproject/abstract-provider';
+import { ImSpinner9 } from 'react-icons/im';
 export const GoldListMint = () => {
 	const { isGoldListSaleActive, mintGoldList } = useSafuu();
-	const [isActive, setIsActive] = useState(false);
 	const [nodes, setNodes] = useState({ fullNode: 0, liteNode: 0 });
 	const {
 		mutate: mint,
 		isLoading,
 		isError,
 		error,
-		isSuccess
+		isSuccess,
 	} = useMutation<TransactionReceipt, Error, typeof nodes>((nodes) =>
 		mintGoldList!(nodes.fullNode, nodes.liteNode)
 	);
-	useEffect(() => {
-		isGoldListSaleActive?.().then(setIsActive);
-	}, [isGoldListSaleActive]);
-	const handleMint = () => {
-		mint(nodes);
-	};
+	const { data: isActive } = useQuery(['goldlist-active', isGoldListSaleActive], () =>
+		isGoldListSaleActive!()
+	);
 	const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
 		setNodes({ ...nodes, [e.target.name]: Number(e.target.value) });
 	};
@@ -41,9 +38,10 @@ export const GoldListMint = () => {
 				onChange={handleChange}
 			/>
 			<div>
-				<button disabled={!isActive} onClick={handleMint}>
+				<button disabled={!isActive || isLoading} onClick={() => mint(nodes)}>
 					Mint
 				</button>
+				{isLoading && <ImSpinner9 className="icon-spin" />}
 				{isError && <div style={{ color: 'red' }}>{error?.message}</div>}
 				{isSuccess && <div style={{ color: 'green' }}>Your nodes have been allocated!</div>}
 			</div>
--- apps/bsc/safuux/components/white-list.tsx
+++ apps/bsc/safuux/components/white-list.tsx
@@ -1,22 +1,24 @@
 import { useSafuu } from '@decentology/hyperverse-bsc-safuu/react';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { ChangeEventHandler, SyntheticEvent, useEffect, useState } from 'react';
+import { TransactionReceipt } from '@ethersproject/abstract-provider';
+import { ImSpinner9 } from 'react-icons/im';
 
 export const WhiteListMint = () => {
 	const { isWhiteListSaleActive, mintWhiteList } = useSafuu();
-	const [isActive, setIsActive] = useState(false);
 	const [nodes, setNodes] = useState({ fullNode: 0, liteNode: 0 });
-	useEffect(() => {
-		isWhiteListSaleActive?.().then(setIsActive);
-	}, [isWhiteListSaleActive]);
-	const handleMint = () => {
+	const {
+		mutate: mint,
+		isLoading,
+		isError,
+		error,
+		isSuccess,
+	} = useMutation<TransactionReceipt, Error, typeof nodes>((nodes) =>
 		mintWhiteList!(nodes.fullNode, nodes.liteNode)
-			.then(() => {
-				// Update UI
-			})
-			.catch((err) => {
-				// Update Error UI
-			});
-	};
+	);
+	const { data: isActive } = useQuery(['whitelist-active', isWhiteListSaleActive], () =>
+		isWhiteListSaleActive!()
+	);
 	const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
 		setNodes({ ...nodes, [e.target.name]: Number(e.target.value) });
 	};
@@ -37,9 +39,12 @@ export const WhiteListMint = () => {
 				onChange={handleChange}
 			/>
 			<div>
-				<button disabled={!isActive} onClick={handleMint}>
+				<button disabled={!isActive} onClick={() => mint(nodes)}>
 					Mint
 				</button>
+				{isLoading && <ImSpinner9 className="icon-spin" />}
+				{isError && <div style={{ color: 'red' }}>{error?.message}</div>}
+				{isSuccess && <div style={{ color: 'green' }}>Your nodes have been allocated!</div>}
 			</div>
 		</div>
 	);
