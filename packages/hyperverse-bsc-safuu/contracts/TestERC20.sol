9800b1216e23cf15375b75f4ae1fca692067fa87
Jonathan Sheely <jsheely@thirtytech.net>
2022-09-03 10:08:07 -0400

    Larger number of tokens to test with

--- packages/hyperverse-bsc-safuu/contracts/TestERC20.sol
+++ packages/hyperverse-bsc-safuu/contracts/TestERC20.sol
@@ -7,7 +7,7 @@ contract TestERC20 is ERC20 {
 
     constructor(
     ) ERC20("Test", "TEST") {
-        _mint(msg.sender, 100000);
+        _mint(msg.sender, 270000000000000000000);
     }
 
     function mint(address _to, uint256 _count) external{
